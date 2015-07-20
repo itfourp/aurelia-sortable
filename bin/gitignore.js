334b401ef2b42c981de02c5bf4499a5be5b52169
stoffeastrom <stoffeastrom@gmail.com>
2015-07-21 00:26:08 +0200

    update

--- bin/gitignore.js
+++ bin/gitignore.js
@@ -2,4 +2,4 @@
 var fs = require("fs-extra");
 var path = require("path");
 
-fs.outputFileSync(path.resolve(__dirname, "../deploy/.gitignore"), "node_modules\n.DS_STORE");
+fs.outputFileSync(path.resolve(__dirname, "../deploy/.gitignore"), "node_modules\n.DS_STORE\npackage.json");
