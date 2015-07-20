b3d61fd6c21143a6df11323ea146d88f31a1d9d0
stoffeastrom <stoffeastrom@gmail.com>
2015-07-21 00:24:58 +0200

    update

--- bin/push.js
+++ bin/push.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
 
+shell.cd("deploy");
 shell.exec("git init");
 shell.exec("git remote add origin https://github.com/oribella/aurelia-sortable.git");
 shell.exec("git add . && git commit -m 'Deploy to gh-pages'");
