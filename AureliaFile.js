73245584d68493a7e980b152b0cda9fb203cc1df
stoffeastrom <stoffeastrom@gmail.com>
2015-08-12 21:19:46 +0200

    updates

--- AureliaFile.js
+++ AureliaFile.js
@@ -1,34 +1,34 @@
-var aurelia = require("aurelia-cli");
+var cli = require('aurelia-cli');
 
- aurelia.command("bundle", {
-   js: {
-     "dist/app-bundle": {
-       modules: [
-         "*",
-         "aurelia-bootstrapper",
-         "aurelia-fetch-client",
-         "aurelia-router",
-         "aurelia-animator-css",
-         "github:aurelia/templating-binding@0.13.2",
-         "github:aurelia/templating-resources@0.13.4",
-         "github:aurelia/templating-router@0.14.1",
-         "github:aurelia/loader-default@0.9.3",
-         "github:aurelia/history-browser@0.6.2",
-         "github:oribella/aurelia-sortable@master",
-         "github:oribella/default-gestures@master"
-       ],
-       options: {
-         inject: true,
-         minify: true
-       }
-     }
-   },
-   template: {
-     "dist/app-bundle": {
-       pattern: "dist/*.html",
-       options: {
-         inject: true
-       }
-     }
-   }
- });
+var bundleCfg = {
+  js: {
+    "dist/app-bundle": {
+      modules: [
+        '*',
+        'aurelia-bootstrapper',
+        'aurelia-fetch-client',
+        'aurelia-router',
+        'aurelia-animator-css',
+        'github:aurelia/templating-binding',
+        'github:aurelia/templating-resources',
+        'github:aurelia/templating-router',
+        'github:aurelia/loader-default',
+        'github:aurelia/history-browser'
+      ],
+      options: {
+        inject: true
+      }
+    }
+  },
+  template: {
+    "dist/app-bundle": {
+      pattern: 'dist/*.html',
+      options: {
+        inject: true
+      }
+    }
+  }
+};
+
+cli.command('bundle', bundleCfg);
+cli.command('unbundle', bundleCfg);
